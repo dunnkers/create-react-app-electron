d2ccdd0ab7e9ceea051e2fa381474f3d87fe6431
dunnkers <jeroenoverschie@gmail.com>
2018-06-04 20:53:16 +0200

    Some interaction with Github API 🌐

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
-import { Button } from 'antd';
+import { Button, Input, Spin, message, List } from 'antd';
 
 import logo from './logo.svg';
 import './App.css';
+
+const Search = Input.Search;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      count: 0,
+      loading: false,
+      data: null
     };
   }
 
   changeCount(number) {
-    const count = this.state.count + number;
+    this.setState({
+      ...this.state,
+      count: this.state.count + number
+    });
+  }
 
-    this.setState({ count });
+  search(username) {
+    if (!username) {
+      return;
+    }
+
+    this.setState({
+      ...this.state,
+      loading: true
+    });
+
+    fetch(`https://api.github.com/users/${username}/repos`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          ...this.state,
+          loading: false,
+          data
+        });
+      }, error => {
+        this.setState({
+          ...this.state,
+          loading: false
+        });
+
+        message.error(error.message);
+      });
   }
 
   render() {
@@ -31,6 +64,30 @@ class App extends Component {
           <Button onClick={() => this.changeCount(-1)}>decrease</Button>
         </p>
         <p>{this.state.count}</p>
+        <div className="App-repo-search">
+          View Github user repos:
+          <Spin spinning={this.state.loading}>
+            <Search
+              placeholder="Github username…"
+              onSearch={username => this.search(username)}
+              enterButton
+            />
+          </Spin>
+          <List
+            bordered
+            dataSource={this.state.data || []}
+            renderItem={item => (
+              <List.Item actions={[
+                <a href={item.html_url + '/stargazers'}>
+                  {item.stargazers_count} ⭐️
+                </a>,
+                <a href={item.html_url}>🌐</a>
+              ]}>
+                <a href={item.url}>{item.name}</a>
+              </List.Item>
+            )}
+          />
+        </div>
       </div>
     );
   }
