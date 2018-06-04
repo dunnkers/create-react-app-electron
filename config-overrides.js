ed1bdd72a23e5df0ec0cf41c750550603aef6974
dunnkers <jeroenoverschie@gmail.com>
2018-06-04 19:53:32 +0200

    Add antd customization support using LESS 💎

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,14 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
     config = rewireReactHotLoader(config, env);
-    config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
+    config = injectBabelPlugin(['import', {
+        libraryName: 'antd', style: true
+    }], config);  // change importing css to less
+    config = rewireLess.withLoaderOptions({
+        modifyVars: { "@primary-color": "#1DA57A" },
+    })(config, env);
     return config;
 }
